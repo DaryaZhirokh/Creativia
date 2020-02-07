988020c472fe5c00ceb51f74b155348d3a358c50
Darya <daraziroh@gmail.com>
2020-02-07 09:45:10 +0300

    header added

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
+  var leftArrow = $("#leftArrow");
+  var rightArrow = $("#rightArrow");
+  var headBackground = $("#headBackground");
 
+  leftArrow.on('click', function() {
+    headBackground.css({"background-image": "url(images/redlady_3.jpg)"})
+
+  });
+
+   rightArrow.on('click', function() {
+    headBackground.css({"background-image": "url(images/redlady.jpg)"})
+    return false;
+  });
 
 });
 
