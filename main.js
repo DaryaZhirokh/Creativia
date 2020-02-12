4dfa64a87791905d5e6464c203b42fa04e8d5715
Darya <daraziroh@gmail.com>
2020-02-12 09:55:32 +0300

    infopage added

--- main.js
+++ main.js
@@ -14,6 +14,48 @@ $(document).ready(function(){
     return false;
   });
 
+   $('.info-item').click(function(){
+      $(this).find(".radius").toggleClass( "radius-toggle" );
+      $(this).find(".info-title").toggleClass( "info-title-toggled" );
+      $(this).find(".info-text").toggleClass( "info-text-toggled" );
+    });
+
+  $('.strength-item').click(function(){
+      $(this).find(".hexagon").toggleClass("hexagon-toggled" );
+      let innerHexagon = $(this).find(".inner-hexagon");
+      if(innerHexagon.hasClass("heart")) {
+        innerHexagon.toggleClass("heart-toggled")
+      }
+      else if (innerHexagon.hasClass("tick")) {
+        innerHexagon.toggleClass("tick-toggled")
+      }
+       else if (innerHexagon.hasClass("ipad")) {
+        innerHexagon.toggleClass("ipad-toggled")
+      }
+      else if (innerHexagon.hasClass("fire")) {
+        innerHexagon.toggleClass("fire-toggled")
+      }
+      else console.log('Noop')
+    });
+
+
+    // $('.strength-item').click(function(){
+    //   $(this).find(".inner-hexagon-tick").toggleClass(".inner-hexagon-tick-toggled" );
+    // });
+
+    // $('.strength-item').click(function(){
+    //   $(this).find(".inner-hexagon-ipad").toggleClass(".inner-hexagon-ipad-toggled" );
+    // });
+
+    // $('.strength-item').click(function(){
+    //   $(this).find(".inner-hexagon-fire").toggleClass(".inner-hexagon-fire-toggled" );
+    // });
+
+    // $('.strength-item').click(function(){
+    //   $(this).find(".inner-hexagon-heart").toggleClass(".inner-hexagon-heart-toggled" );
+    // });
+
+
+
+
 });
-
-
